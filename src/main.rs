b9eadc642a48980f2b908cb18fe11941f1722f35
Killian Hourihan <kllnhrhn@gmail.com>
2024-04-27 17:44:03 -0700

    Added basic CLI prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use rustyline::{DefaultEditor, error::ReadlineError};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdin = DefaultEditor::new().unwrap();
+
+    println!();
+
+    loop {
+        match stdin.readline(">> ") {
+            Ok(input) => {
+                println!("{}", input);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("^D");
+                break;
+            },
+            Err(err) => {
+                println!("stdin error: {}", err);
+                break;
+            }
+        }
+    }
 }
